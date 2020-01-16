f9e00369e4303d59fe53cc5a00ca677303057e69
danilomalzao <danilomalzao@hotmail.com>
2020-01-16 19:23:17 -0300

    QGIS - 16/01/2020 19:22:53

---
+++ startHttps.bat
@@ -0,0 +1,4 @@
+rem C:\Danilo\Temp\ngrok.exe http -host-header=rewrite 8887
+rem C:\Danilo\Temp\ngrok.exe http file:///C/Export/Trampo/modelos/GitHubWMS/WMS4
+C:\Danilo\Temp\ngrok.exe http 8887
+chrome chrome://apps
